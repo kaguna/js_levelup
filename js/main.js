ee6211e75d4a684831241a117fe555f19729995d
kaguna <karyorkir@gmail.com>
2019-01-16 16:38:34 +0300

    Code structure

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*
+Code Structure in javascript
+Statements, semicolons and comments are some of the 
+buliding blocks js code. 
+*/
+alert("This code implements statements in the code structure.");
+// The statement above highlights some building blocks.
