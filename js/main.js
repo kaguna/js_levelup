5e9b4a870160efc9c0885f70c954f8eb63622936
kaguna <karyorkir@gmail.com>
2019-01-16 17:03:26 +0300

    Coding style

--- js/main.js
+++ js/main.js
@@ -3,5 +3,29 @@ Code Structure in javascript
 Statements, semicolons and comments are some of the 
 buliding blocks js code. 
 */
-alert("This code implements statements in the code structure.");
+alert(`This code implements statements 
+        in the code structure.`);
 // The statement above highlights some building blocks.
+
+/*
+Code Style:-Syntax
+*/
+function pow(x, n) { // space between parameters and curly bracket on the same line after space.
+	let result = 1 //identation of 2 spaces
+	
+	for (let i = 0; i < n; i++) {// space after for/if/while & other loops and a semicolon is mandatory at the end.
+	result *= x;
+	}
+	
+	return result;
+}
+
+let x = prompt("x?", "");
+let n = prompt("n?", "");
+// An empty line between logical blocks
+if (n < 0) {
+    alert(`Power ${n} is not supported, please 
+        enter an integer number, greater then 0`);// short lines(80 to 100 chars)
+} else {//else without a line break.
+	alert( pow(x,n) );//spaces around a nested call. 
+}
