da0ed11637435392e95b4aab808b4daf7fba609e
kaguna <karyorkir@gmail.com>
2019-03-21 13:27:21 +0300

    Strings

--- dataTypes.js
+++ dataTypes.js
@@ -104,3 +104,126 @@ DATATYPES
     console.log( Math.min(1, 2) ); // 1
 
     console.log( Math.pow(2, 10) ); // 2 in power 10 = 1024
+
+/*
+    STRINGS
+        The internal format for strings is always UTF-16, it is not tied to the page encoding
+
+        Quotes
+            let single = 'single-quoted';
+            let double = "double-quoted";
+            let backticks = `backticks`;
+    
+        Special characters
+            \b - Backspace
+            \f - Form feed
+            \n - New line
+            \r - Carriage return
+            \t - Tab
+            \uNNNN	A unicode symbol with the hex code NNNN, for instance \u00A9 – is a unicode for the copyright symbol ©. 
+                It must be exactly 4 hex digits.
+            \u{NNNNNNNN}	Some rare characters are encoded with two unicode symbols, taking up to 4 bytes. 
+                This long unicode requires braces around it.
+            Examples:
+*/
+            console.log( "\u0147" ); // Ň
+            console.log( "\u{1F64F}" ); // 🙏, folded hands (another long unicode)
+/*
+        String length
+            The length property has the string length:
+*/           console.log( `James/kaguna`.length ); // 12
+/*
+        Accessing characters
+            To get a character at position pos, use square brackets [pos] or call the method str.charAt(pos). 
+                The first character starts from the zero position:
+*/
+            let name = `James Kariuki`;
+
+            // the first character
+            console.log( name[0] ); // J
+            console.log( name.charAt(4) ); // s
+
+            // the last character
+            console.log( name[name.length - 1] ); // i
+/*
+        Strings are immutable i.e. Strings can’t be changed in JavaScript. It is impossible to change a character.
+        The usual workaround is to create a whole new string and assign it to "name" instead of the old one.
+
+
+        Searching for a substring:
+            To look for a substring, use: indexOf, or includes/startsWith/endsWith for simple checks.
+*/
+        let description = 'James is a footballer.';
+
+        console.log( description.indexOf('James') ); // 0, because 'James' is found at the beginning
+        console.log( description.indexOf('james') ); // -1, not found, the search is case-sensitive
+
+        console.log( description.indexOf("footballer") ); // 11, "footballer" is found at the position 11
+/*
+        includes - returns true/false depending on whether str contains substr within.
+            str.includes("substr")
+        startsWith - checks whether a str starts with a substring
+             str.startsWith("substr")
+        endsWith -  checks whether a str ends with a substring
+             str.endsWith("substr")
+
+/*
+        Getting a substring
+            To get a substring, use: slice, substr or substring.
+            str.slice(start [, end])
+            str.substring(start [, end])
+            str.substr(start [, length])
+
+            slice(start, end)	        from start to end (not including end)	    allows negatives
+            substring(start, end)	    between start and end	                    negative values mean 0
+            substr(start, length)	    from start get length characters	        allows negative start
+*/
+        console.log( description.slice(3, 8) ); // es is (between pos 3 & 8 not including 3 & 8)
+        console.log( description.slice(3) ); // es is a footballer.(from pos till end)
+        console.log( description.slice(-8, -2) ); // ler.(start at the 8th position from the right, end at the 2nd from the right)
+
+        console.log( description.substring(2, 6) ); // mes
+        console.log( description.substring(6, 2) ); // mes. NB: this does not apply to slice.
+
+        console.log( description.substr(2, 6) ); // mes is
+/*
+        Comparing strings
+            strings are compared character-by-character in alphabetical order.
+            To compare strings according to the language, use: localeCompare, otherwise they are compared by character codes.
+            
+            str.codePointAt(pos) - Returns the code for the character at position pos
+            String.fromCodePoint(code) - Creates a character by its numeric code
+*/
+        console.log( "z".codePointAt() ); // 122
+        console.log( "Z".codePointAt() ); // 90
+
+        console.log( String.fromCodePoint(35) ); // #
+        console.log( String.fromCodePoint(90) ); // Z
+
+        let string = '';
+
+        for (let i = 65; i <= 220; i++) {
+        string += String.fromCodePoint(i);
+        }
+        console.log( string );
+        // ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~
+        // ¡¢£¤¥¦§¨©ª«¬­®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜ
+/*
+        Correct comparisons
+            The call str.localeCompare(str2):
+                - Returns 1 if str is greater than str2 according to the language rules.
+                - Returns -1 if str is less than str2.
+                - Returns 0 if they are equal.
+*/       
+        console.log( 'james'.localeCompare('james') ); // 0
+        console.log( 'jamesk'.localeCompare('james') ); // 1
+        console.log( 'james'.localeCompare('jamesk') ); // -1
+/*
+        Surrogate pairs
+            Most symbols have a 2-byte code. Letters in most european languages, numbers, 
+                and even most hieroglyphs, have a 2-byte representation.
+            But 2 bytes only allow 65536 combinations and that’s not enough for every possible symbol.
+                 So rare symbols are encoded with a pair of 2-byte characters called “a surrogate pair”.
+            The length of such symbols is 2:
+*/
+        console.log( '❤️'.length ); // 2, Heart
