b12b2fa38cd0712dc713258dd3b88894b98ac584
kaguna <karyorkir@gmail.com>
2019-04-30 15:44:02 +0300

    Event delegation

--- introductionIntoEvents.js
+++ introductionIntoEvents.js
@@ -95,3 +95,25 @@
         Bubbling and capturing lay the foundation for “event delegation” – an extremely powerful
          event handling pattern
 */
+
+/*
+    Event delegation
+        It’s often used to add same handling for many similar elements, but not only for that.
+
+        The algorithm:
+            - Put a single handler on the container.
+            - In the handler – check the source element event.target.
+            - If the event happened inside an element that interests us, then handle the event.
+
+        Benefits:
+            - Simplifies initialization and saves memory: no need to add many handlers.
+            - Less code: when adding or removing elements, no need to add/remove handlers.
+            - DOM modifications: we can mass add/remove elements with innerHTML and alike.
+        
+        Limitations:
+            - First, the event must be bubbling. Some events do not bubble. Also, low-level
+              handlers should not use event.stopPropagation().
+            - Second, the delegation may add CPU load, because the container-level handler reacts
+              on events in any place of the container, no matter if they interest us or not. But
+               usually the load is negligible, so we don’t take it into account.     
+*/
