9a2b08517edab85c3c0ffadfbaca0dd72072a633
kaguna <karyorkir@gmail.com>
2019-04-30 14:55:27 +0300

    Bubbling and capturing

--- introductionIntoEvents.js
+++ introductionIntoEvents.js
@@ -70,3 +70,28 @@
             No matter how you assign the handler – it gets an event object as the first argument.
              That object contains the details about what’s happened.
 */
+
+/*
+    Bubbling and capturing
+        The event handling process:
+            - When an event happens – the most nested element where it happens gets labeled as the
+              “target element” (event.target).
+            - Then the event first moves from the document root down to the event.target, calling
+              handlers assigned with addEventListener(...., true) on the way (true is a shorthand
+                 for {capture: true}).
+            - Then the event moves from event.target up to the root, calling handlers assigned
+              using on<event> and addEventListener without the 3rd argument or with the 3rd
+               argument false.
+
+        Each handler can access event object properties:
+            - event.target – the deepest element that originated the event.
+            - event.currentTarget (=this) – the current element that handles the event
+              (the one that has the handler on it)
+            - event.eventPhase – the current phase (capturing=1, bubbling=3).
+
+        Any event handler can stop the event by calling event.stopPropagation(), but that’s not
+         recommended, because we can’t really be sure we won’t need it above
+
+        Bubbling and capturing lay the foundation for “event delegation” – an extremely powerful
+         event handling pattern
+*/
